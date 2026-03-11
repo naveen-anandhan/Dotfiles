c5caa75c38ecf3a90d169cf48b2ce2a473cbdf57
Naveen Anandhan <160575974+naveen-anandhan@users.noreply.github.com>
2026-03-11 20:48:27 +0530

    Delete naveen_dotfile/nvim directory

--- naveen_dotfile/nvim/lua/config/keymaps.lua
+++
@@ -1,8 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
---
---
--- This version forces Telescope to load if it hasn't already
--- Standard LazyVim way to open the theme switcher
-vim.keymap.set("n", "<leader>tt", "<cmd>Telescope colorscheme<cr>", { desc = "Theme Switcher" })
