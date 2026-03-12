d542e722146c2322a1ff92e8833accc9bb392958
Naveen Anandhan <160575974+naveen-anandhan@users.noreply.github.com>
2026-03-12 09:59:28 +0530

    Update cursor.lua

--- nvim/lua/plugins/cursor.lua
+++ nvim/lua/plugins/cursor.lua
@@ -1,11 +1,33 @@
 return {
   "sphamba/smear-cursor.nvim",
-
   opts = {
-    smear_between_buffers = true,
-    smear_between_neighbor_lines = true,
-    scroll_buffer_space = true,
-    legacy_computing_symbols_support = false,
-    smear_insert_mode = true,
+    cursor_color = "#E6CDFA",
+    particles_enabled = true,
+    particles_max_num = 1200,
+
+    -- Emission Settings
+    particles_per_second = 1000,
+    particles_per_length = 50,
+    particle_max_lifetime = 1200,
+    min_distance_emit_particles = 0,
+
+    -- Fire Physics
+    particle_gravity = -60,
+    particle_velocity_from_cursor = 0.8,
+    particle_max_initial_velocity = 20,
+    particle_spread = 1.2,
+    particle_damping = 0.1,
+
+    -- Aesthetics
+    gradient_exponent = 0,
+    gamma = 1,
+    never_draw_over_target = true,
+    hide_target_hack = true,
+
+    -- Movement Dynamics
+    stiffness = 0.5,
+    trailing_stiffness = 0.2,
+    trailing_exponent = 5,
+    damping = 0.6,
   },
 }
