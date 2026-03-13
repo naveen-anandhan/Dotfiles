e68e57f0e47c164e6a977951daa6c9d80ed059d9
Naveen Anandhan <160575974+naveen-anandhan@users.noreply.github.com>
2026-03-13 12:57:46 +0530

    updated configs

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>tt", "<cmd>Telescope colorscheme<cr>", { desc = "Theme Switcher" })
+vim.keymap.set("i", "<S-Tab>", "<C-d>", { desc = "Unindent" })
+vim.keymap.set("n", "<S-Tab>", "<<", { desc = "Unindent line" })
+vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Unindent selection" })
