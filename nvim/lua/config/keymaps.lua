16cbb36ea15dc39eb9797364a26242526b68dc92
Naveen Anandhan <160575974+naveen-anandhan@users.noreply.github.com>
2026-03-14 11:02:15 +0530

    update dotfiles configuration

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,3 +5,9 @@ vim.keymap.set("n", "<leader>tt", "<cmd>Telescope colorscheme<cr>", { desc = "Th
 vim.keymap.set("i", "<S-Tab>", "<C-d>", { desc = "Unindent" })
 vim.keymap.set("n", "<S-Tab>", "<<", { desc = "Unindent line" })
 vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Unindent selection" })
+
+
+vim.keymap.set("i", "<C-h>", "<Left>",  { noremap = true })
+vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true })
+vim.keymap.set("i", "<C-j>", "<Down>",  { noremap = true })
+vim.keymap.set("i", "<C-k>", "<Up>",    { noremap = true })
--- nvim/lua/plugins/cursor.lua
+++ nvim/lua/plugins/cursor.lua
@@ -5,18 +5,18 @@ return {
       cursor_color = "#E6CDFA",
 
       particles_enabled = true,
-      particles_max_num = 1250,
+      particles_max_num = 200,
 
-      particles_per_second = 500,
+      particles_per_second = 100,
       particles_per_length = 25,
-      particle_max_lifetime = 900,
+      particle_max_lifetime = 200,
 
       min_distance_emit_particles = 2, -- prevents smoke over text
 
       particle_gravity = -60,
       particle_velocity_from_cursor = 0.8,
-      particle_max_initial_velocity = 20,
-      particle_spread = 1.2,
+      particle_max_initial_velocity = 10,
+      particle_spread = 0.5,
       particle_damping = 0.1,
 
       gradient_exponent = 0,
@@ -27,7 +27,7 @@ return {
       stiffness = 0.5,
       trailing_stiffness = 0.2,
       trailing_exponent = 5,
-      damping = 0.6,
+      damping = 0.2,
     },
   },
 }
