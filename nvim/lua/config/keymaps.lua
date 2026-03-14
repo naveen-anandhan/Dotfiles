90c5da7852e9f52d8d4bbf82513aa707e0976e2d
Naveen Anandhan <160575974+naveen-anandhan@users.noreply.github.com>
2026-03-14 14:49:51 +0530

    update dotfiles

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -11,3 +11,10 @@ vim.keymap.set("i", "<C-h>", "<Left>",  { noremap = true })
 vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true })
 vim.keymap.set("i", "<C-j>", "<Down>",  { noremap = true })
 vim.keymap.set("i", "<C-k>", "<Up>",    { noremap = true })
+
+-- In init.lua
+vim.keymap.set({'n', 'v'}, 'd', '"_d')
+vim.keymap.set({'n', 'v'}, 'dd', '"_dd')
+
+vim.keymap.set("n", "<leader>r", ":w | belowright split | terminal python3 %<CR>")
+
